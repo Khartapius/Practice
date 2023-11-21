5f96b108cec40dc2a9cb7b0d2e534165ef973702
Chiir-Deri Pius <chemogohpius@gmail.com>
2023-11-21 08:59:43 +0000

    Purchasing Airtime

--- tests/hub.spec.js
+++ tests/hub.spec.js
@@ -15,16 +15,27 @@ test('Checking the Hubtel Food page', async({page, context}) => {
     expect(await page.title()).toBe("Hubtel - Find and pay for everyday essentials");
     
     // const restaurantName = 'KFC';
-    //waiting for loading of the foods
-    await page.waitForLoadState('networkidle')
-    await page.getByRole('button', { name: 'Change Location' }).click();
-    await page.getByText('Add a new location').click();
+    
+    await page.waitForLoadState('networkidle');
+    await page.getByText('Airtime, Data & Bills').click();
+    await page.getByRole('tab', {name: 'History'}).click();
+    await page.locator(".col-12 a").last().click();
+    await page.locator("[placeholder='0.00']").fill("1");
+    await page.getByRole('button', { name: ' NEXT '}).click();
+    await page.waitForLoadState('networkidle');
+    await page.locator("[value='hubtel_wallet']").click();
+    await page.getByRole('button', { name: ' PAY NOW '}).click();
+    await page.getByRole('link', {name: ' I have paid for this '}).click();
 
-    await page.locator("[placeholder='Search for places']").type("Dansoman Roundabout, Accra, Ghana",{delay:1000});
-    await page.locator("[placeholder='Enter name']").type("Dansoman Roundabout",{delay:1000});
+    await page.pause();
+    // await page.getByText('Add a new location').click();
 
-    await page.getByRole('button', { name: 'Save'}).click();
-    page.pause();
+    // const location = await page.locator("[placeholder='Search for places']").type("Dansoman Roundabout, Accra, Ghana",{delay:100});
+    // await location.getByText('Dansoman RoundaboutAccra, Ghana').click();
+    // await page.locator("[placeholder='Enter name']").type("Dansoman Roundabout",{delay:100});
+
+    // await page.getByRole('button', { name: 'Save'}).click();
+    // await page.pause();
    
     
 });
