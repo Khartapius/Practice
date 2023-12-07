fdf90186fc4b96d9e8cfd942054236ee2de78ed6
Chiir-Deri Pius <chemogohpius@gmail.com>
2023-12-07 09:55:52 +0000

    Updating codes for visual testing

--- tests/Visual_Testing/screenshot.spec.js
+++ tests/Visual_Testing/screenshot.spec.js
@@ -10,21 +10,31 @@ test('Accessing the Hubtel page using the QRcode', async({browser}) => {
 
     // Accessing the hubtel page
     await page.goto("https://hubtel.com");
+    const title = await expect(page).toHaveTitle('Hubtel - Find and pay for everyday essentials');
+    console.log(title);
     await page.waitForLoadState('networkidle');
 
     //Navigating to Everyday Essentials page    
-    await page.getByRole('link', {name: 'Everyday Essentials'}).click();
+    await page.getByRole('link', {name: 'Everyday Essentials'}).first().click();
     await page.getByText('Non Alcoholic Drinks').click();
-    await page.waitForLoadState("networkidle");
+    await page.waitForLoadState("load");
 
-    //Select a product in the list of products
+    //Searching for a product using the search bar
+    await page.locator(".form-control").type("welch's", {delay:1000});
+    await page.locator("[type='submit']").click();
+    
     const names = await page.locator(".card-body h6").allTextContents();
     await page.waitForLoadState("networkidle");
-    await page.getByRole('link', { name: 'product Welch\'s Fruit Juice 473ml GHS 35.00' }).click();    
+    await expect(page.locator("h5 span")).toHaveText(" welch's ");
+    expect(await page.getByRole('link', { name: 'product Welch\'s Fruit Juice 473ml GHS 35.00' }).click());
     await page.locator(".add-cart-button").click();
+    await page.waitForLoadState("networkidle");
+
+    //Confirming your order
+    await page.locator(".alert").first().click();
 
     //Taking screenshot of the product added to cart
-    await page.locator("[alt='product-image']").first().screenshot({path: 'product1.png'});
+    expect(await page.screenshot()).toMatchSnapshot('addedToCart.png');
      await page.pause();
         console.log(names);
    
